5dbd02ba40e23268c4d53ec9597c98d607250902
e <rtnndr04r01i872g@studenti.unical.it>
2026-07-11 00:25:53 +0200

    inizio keycloak

--- StoreFE/src/app/security/keycloak/keycloak.service.ts
+++ StoreFE/src/app/security/keycloak/keycloak.service.ts
@@ -5,27 +5,21 @@ import Keycloak from 'keycloak-js';
   providedIn: 'root'
 })
 export class KeycloakService {
-  private _keycloak: Keycloak | undefined;
 
-  get keycloak() {
-    if(!this.keycloak) {
-      this._keycloak = new Keycloak({
-        url: 'http://localhost:9090',
-        realm: 'store',
-        clientId: 'prod'
-      });
-    }
-    return this._keycloak;
-  }
   constructor() { }
 
-  async init() {
-    console.log('Initializing Keycloak');
-    const authenticated = await this.keycloak?.init({
-      onLoad: "login-required"
-    })
-    if (authenticated) {
-      console.log('Authenticated');
-    }
+  private keycloak!: Keycloak;
+
+  init(): Promise<boolean> {
+    this.keycloak = new Keycloak({
+      url: "http://localhost:9090",
+      realm:'store',
+      clientId: 'prod'
+    });
+
+    return this.keycloak.init({
+      onLoad: 'check-sso',
+      checkLoginIframe: false,
+    });
   }
 }
