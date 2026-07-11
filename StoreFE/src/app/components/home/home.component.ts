0def943e73af96d3f0032e3fedbcedda22101ed0
e <rtnndr04r01i872g@studenti.unical.it>
2026-07-11 17:32:28 +0200

    landing page

--- StoreFE/src/app/components/home/home.component.ts
+++ StoreFE/src/app/components/home/home.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {KeycloakService} from '../../security/keycloak/keycloak.service'
+import { ProductService } from '../../services/product.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  constructor(public keycloakService: KeycloakService) {
+
+export class HomeComponent implements OnInit {
+  products: any[] = [];
+  isLogged: boolean = false;
+
+  constructor(
+    public productService: ProductService,
+    public keycloakService: KeycloakService
+  ) {}
+
+  ngOnInit(): void {
+    this.isLogged = this.keycloakService.isLoggedIn();
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (data) => this.products = data,
+      error: (err) => console.error('Err', err)
+    });
+  }
+
+  aggiungiAlCarrello(product: any): void {
+    if (!this.isLogged) {
+      this.keycloakService.login();
+      return;
+    }
   }
 }
---
+++ StoreFE/src/app/models/product.model.ts
@@ -0,0 +1,12 @@
+export interface Product {
+
+  id?: number;
+  name: string;
+  category: string;
+  description: string;
+  code:string
+  price: number;
+  quantity: number;
+  imgUrl: string;
+
+}
